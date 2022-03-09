71d9ce715649ca8283e65db96365235a03657b6d
emre-unaldi <emree.unaldi@gmail.com>
2022-03-09 22:34:13 +0300

    Creating a server with the Fastify package

---
+++ shopping-basket-api/app.js
@@ -0,0 +1,26 @@
+const fastify = require('fastify')({ logger: true });
+const dotenv = require('dotenv').config({ path: './src/config/.env' });
+
+const port = process.env.PORT || 3002;
+fastify.register(require('fastify-cors'));
+
+fastify.get('/', async (req, reply) => {
+    return { message: 'Hello Fastify'}
+});
+
+const startServer = async () => {
+    try {
+        await fastify
+            .listen(port)
+            .then(() => {
+                console.log(`Server started on port ${port}`);
+            }).catch((err) => {
+                console.log(`Server failed to start on port ${port}`);
+            });
+    } catch (err) {
+        fastify.log.error(err);
+        process.exit(1)
+    }
+};
+
+startServer();
